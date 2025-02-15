2b0da8a5be39fb530f6bc85cf9e9d23ef787d554
herveynavarro04 <herveynavarro2004@gmail.com>
2025-02-14 18:18:13 -0600

    login started

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,21 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function Index() {
-  const [message, setMessage] = useState("Loading...");
+  // const [message, setMessage] = useState("Loading...");
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  
+  // useEffect(() => {
+  //   const fetchUrl = process.env.FETCH_URL || "http://localhost:8080/api";
+  //   fetch(fetchUrl)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setTimeout(() => {
+  //         setMessage(data.message);
+  //       }, 3000);
+  //     });
+  // }, []);
 
-  useEffect(() => {
-    const fetchUrl = process.env.FETCH_URL || "http://localhost:8080/api";
-    fetch(fetchUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setTimeout(() => {
-          setMessage(data.message);
-        }, 3000);
-      });
-  }, []);
+  const handleSumbit  = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(user);
+    console.log(password)
+  }
 
-  return <div>{message}</div>;
+  return (
+    <div>
+      <div className="logo"></div>
+      <div className="container">
+        <h2>Sign In</h2>
+        <input
+          className="formData"
+          placeholder="user"
+          type="text"
+          id="fname"
+          name="fname"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        /><br />
+        <input
+          className="formData"
+          placeholder="password"
+          type="password"
+          id="lname"
+          name="lname"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        /><br />
+        <button onClick={handleSumbit}>sign in</button>
+      </div>
+    </div>
+  );
 }
 
 export default Index;
