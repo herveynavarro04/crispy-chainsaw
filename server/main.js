853f4557193cc6f758d8567ee890297b24f96fc7
herveynavarro04 <herveynavarro2004@gmail.com>
2025-03-07 17:09:43 -0600

    hot fix

--- server/main.js
+++ server/main.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello World !" });
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello World !' });
 });
 
 app.listen(port, () => {
