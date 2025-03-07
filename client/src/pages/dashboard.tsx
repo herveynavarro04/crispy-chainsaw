bc75fe2ec11135325be31ae1f4fe8a34bf9f043c
herveynavarro04 <herveynavarro2004@gmail.com>
2025-03-07 16:41:29 -0600

    hot fix

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -3,22 +3,22 @@ import React, { useEffect, useState } from "react";
 
 export default function Dashboard() {
   const router = useRouter();
-  const [isAuth, setIsAuth] = useState<boolean | null>(null); 
+  const [isAuth, setIsAuth] = useState<boolean | null>(null);
 
   useEffect(() => {
-    if (typeof window !== "undefined") { 
-      const auth = localStorage.getItem("IsAuth"); 
+    if (typeof window !== "undefined") {
+      const auth = localStorage.getItem("IsAuth");
 
       if (!auth) {
-        router.replace("/"); 
+        router.replace("/");
       } else {
         setIsAuth(true);
       }
     }
-  }, []);
+  }, [router]);
 
   if (isAuth === null) {
-    return <p>Checking authentication...</p>; 
+    return <p>Checking authentication...</p>;
   }
 
   return (
@@ -26,8 +26,8 @@ export default function Dashboard() {
       <h1>Dashboard</h1>
       <button
         onClick={() => {
-          localStorage.removeItem("isAuthenticated"); 
-          router.replace("/"); 
+          localStorage.removeItem("isAuthenticated");
+          router.replace("/");
         }}
       >
         Logout
