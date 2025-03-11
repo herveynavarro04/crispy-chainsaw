93eed4b55399300ce1cc4d064ec9d8e575987e91
herveynavarro04 <herveynavarro2004@gmail.com>
2025-03-11 14:30:59 -0600

    hot fix

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -34,6 +34,7 @@ function Index() {
           value={user}
           onChange={(e) => setUser(e.target.value)}
         /><br />
+        <br />
         <input
           className="formData"
           placeholder="password"
